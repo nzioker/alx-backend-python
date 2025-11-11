44d795be2d1bfcb27db5e7a0a2672b5ace16a341
nzioker <nzioker@gmail.com>
2025-11-11 15:47:17 +0300

    Refactor test_get_json to use context manager

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -30,12 +30,11 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com",{"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
-    @patch('utils.requests.get')
-    def test_get_json(self, test_url, test_response, mock_requests_get):
-
-        mock_response = Mock()
-        mock_response.json.return_value = test_response
-        mock_requests_get.return_value = mock_response
-        result = get_json(test_url)
-        mock_requests_get.assert_called_once_with(test_url)
-        self.assertEqual(result, test_response)
+    def test_get_json(self, test_url, test_response):
+        with patch('utils.requests.get') as mock_requests_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_response
+            mock_requests_get.return_value = mock_response
+            result = get_json(test_url)
+            mock_requests_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_response)
