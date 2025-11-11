161292c48004c9d5bffe073ede95979c9285872f
nzioker <nzioker@gmail.com>
2025-11-11 15:39:22 +0300

    Add unit tests for get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,3 +23,19 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self,nested_map,path, expected):
         with self.assertRaises(expected):
             access_nested_map(nested_map,path)
+
+class TestGetJson(unittest.TestCase):
+
+    @parameterized.expand([
+        ("http://example.com",{"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_response, mock_requests_get):
+
+        mock_response = Mock()
+        mock_response.json.return_value = test_response
+        mock_requests_get.return_value = mock_response
+        result = get_json(test_url)
+        mock_requests_get.assert_called_once_with(test_url)
+        self.assertEqual(result, test_response)
