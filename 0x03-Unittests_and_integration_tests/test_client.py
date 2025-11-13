0664e170d2b03cc2f5684096d0201279c80c26f8
nzioker <nzioker@gmail.com>
2025-11-13 15:33:41 +0300

    Implement parameterized test for license checking
    Add parameterized test for has_license method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -73,3 +73,12 @@ class TestGithubOrgClient(unittest.TestCase):
 
             mock_get_json.assert_called_once_with(mock_url)
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test GithubOrgClient.has_license returns expected result"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
