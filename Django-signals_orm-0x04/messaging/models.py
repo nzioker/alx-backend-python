c546ad44497de5472912c2fd79913f2775fe14c4
Ken Nzioka <nzioker@gmail.com>
2025-12-06 16:28:45 +0300

    modified models.py

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -9,6 +9,8 @@ class Message(models.Model):
     timestamp = models.DateTimeField(default=timezone.now)
     read = models.BooleanField(default=False)
     edited = models.BooleanField(default=False)
+    edited_at = models.DateTimeField(null=True, blank=True) 
+    edited_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='edited_messages') 
     parent_message = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='replies')
     
     def __str__(self):
@@ -30,6 +32,7 @@ class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
     old_content = models.TextField()
     changed_at = models.DateTimeField(default=timezone.now)
+    changed_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
     
     def __str__(self):
         return f"History for message {self.message.id}"
